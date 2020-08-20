b8aee89128accb8902d682999a8b26726134607a
Enrico John Oliveros <ejsoliveros@gmail.com>
2020-08-20 21:23:06 +0800

    Add weather data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (longitude, latitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location')
         } else {
-            callback(undefined,"Weather forecast is " + body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degrees out. It feels like " + body.current.feelslike + " degrees out.")
+            callback(undefined,"Weather forecast is " + body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degrees out. It feels like " + body.current.feelslike + " degrees out. Precipitation is at " + body.current.precip + ".")
         }
     })
 }
